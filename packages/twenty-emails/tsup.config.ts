e22954b7321e40bd2392a3a8962b4b1f70146eb8
martmull <martmull@hotmail.fr>
2024-01-19 12:37:23 +0100

    WIP Import emails templates properly

--- packages/twenty-emails/tsup.config.ts
+++ packages/twenty-emails/tsup.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     entry: { index: './tsup.index.tsx' },
-    format: ['cjs', 'esm'],
-    dts: true,
-    outDir: '../twenty-server/src/emails/generated',
   },
 ]);
